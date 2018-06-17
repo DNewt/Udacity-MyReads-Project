33b72a6b85231aa1e036581fe0d856ba49ba35ec
David Newton <Davidwillnewton@gmail.com>
2018-06-17 21:55:02 +1200

    Made books get pulled from the backend

--- myreads/src/CurrentlyReading.js
+++ myreads/src/CurrentlyReading.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import Book from './Book'
+import Book from './Book';
+import * as BooksAPI from './BooksAPI'
 
 class CurrentlyReading extends Component {
+
+    componentDidMount() {
+        
+        BooksAPI.getAll().then((book) => book.filter((book) => {
+            return book.shelf === "currentlyReading";
+        })).then(book => this.setState({
+            books: book
+        }))
+
+    }
+    
+    state = {
+        books: []
+    }
+
   render() {
+    console.log(this.state.books)
     return (
       <div className="CurrentlyReading">
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
         <ol className="books-grid">
-            <li>
-                <Book title="To Kill a Mockingbird" author="Harper Lee" url='url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' />
+        {this.state.books.map((book) => 
+            <li> 
+                <Book title={book.title} author={book.authors[0]} url={'url(' + book.imageLinks.thumbnail +')'}/>
             </li>
-            <li>
-                <Book title="Ender's Game" author="Orson Scott Card" url='url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' />
-            </li>
+        )}    
         </ol>
         </div>
       </div>
--- myreads/src/WantToRead.js
+++ myreads/src/WantToRead.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 
 class WantToRead extends Component {
+
+    componentDidMount() {
+        
+        BooksAPI.getAll().then((book) => book.filter((book) => {
+            return book.shelf === "wantToRead";
+        })).then(book => this.setState({
+            books: book
+        }))
+
+    }
+    
+    state = {
+        books: []
+    }
+
+
   render() {
     return (
       <div className="WantToRead">
         <h2 className="bookshelf-title">Want to Read</h2>
         <div className="bookshelf-books">
             <ol className="books-grid">
-                <li>
-                    <Book title="1776" author="David McCullough" url='url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'/>
+            {this.state.books.map((book) => 
+                <li> 
+                    <Book title={book.title} author={book.authors[0]} url={'url(' + book.imageLinks.thumbnail +')'}/>
                 </li>
-                <li>
-                    <Book title="Harry Potter and the Sorcerer's Stone" author="J.K. Rowling" url='url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' />
-                </li>
+             )}  
             </ol>
         </div>
       </div>
